0d3120e91a71eff501198c440f8c040909501f91
wesleymktd <wesleymktd@gmail.com>
2023-06-01 22:56:36 -0300

    test_encripy req 2ok

--- challenges/challenge_encrypt_message.py
+++ challenges/challenge_encrypt_message.py
@@ -1,5 +1,4 @@
 def encrypt_message(message: str, key: int):
-
     if not isinstance(key, int):
         raise TypeError("tipo inválido para key")
 
@@ -12,7 +11,10 @@ def encrypt_message(message: str, key: int):
     part_one = reversed(message[:key])
     part_two = reversed(message[key:])
 
-    if not key % 2:
+    if not key % 2:  # par
         part_two, part_one = part_one, part_two
 
     return "".join(part_one) + "_" + "".join(part_two)
+
+
+# print(encrypt_message("wesley", 2))
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(3, 3)  # message invalid
+
+    with pytest.raises(TypeError):
+        encrypt_message("bola", "P")  # key invalid
+
+    assert encrypt_message("wesley", -1) == "yelsew"  # key negative
+    assert encrypt_message("wesley", 3) == "sew_yel"  # key impar
+    assert encrypt_message("wesley", 2) == "yels_ew"  # key par
