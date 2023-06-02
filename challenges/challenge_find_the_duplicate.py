f860b7b46b5e18e6fbe95c09cd57da8c036cb841
wesleymktd <wesleymktd@gmail.com>
2023-06-02 17:27:20 -0300

    finalizei algorithm duplicate req5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,25 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+from collections import Counter
+
+
+def repeat_major_one(dictionary):
+    filtered_keys = [key for key, value in dictionary.items() if value >= 2]
+    return not len(filtered_keys) <= 1 or filtered_keys == []
+
+
+def find_duplicate(nums=[]):
+
+    if len(nums) <= 1 or not isinstance(nums, list):
+        return False
+
+    counter = Counter(nums)
+
+    if repeat_major_one(counter):
+        return False
+
+    for num in nums:
+        if num < 0:
+            return False
+
+    filtered_keys = [key for key, value in counter.items() if value >= 2]
+
+    return filtered_keys[0]
