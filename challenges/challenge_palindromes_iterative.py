b1fe23138c878f8b720739c396f643f9513ea939
wesleymktd <wesleymktd@gmail.com>
2023-06-02 18:30:38 -0300

    palindrome_iterative req6 ok

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    low_index = 0
+    high_index = len(word) - 1
+
+    if not word:
+        return False
+
+    while low_index <= high_index:
+        if word[low_index] != word[high_index]:
+            return False
+        else:
+            low_index += 1
+            high_index -= 1
+
+    return True
